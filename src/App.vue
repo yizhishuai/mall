48e1cef354331f05676b17bfc7bdfa0dedc69bf8
sysbian <liushuaidejia@qq.com>
2021-04-05 21:33:05 +0800

    local_json and interface setting

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,25 @@
 
 <script>
 //import NavHome from './pages/home.vue'
-import storage from './storage/index'
+//import storage from './storage/index'
 export default {
   name: 'app',
   // components: {
   //   NavHome
   // }
+  data(){
+    return{
+      res:{}
+    }
+  },
   mounted(){
    // storage.setItem('a',1)
    //storage.setItem('user',{a:1})
    //storage.setItem('abc',{a:1},'user')
-   storage.clear('a')
+   //storage.clear('a')
+   this.axios.get('/mock/user/login.json').then((res) => {
+     this.res=res;
+   })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+
+//根据前端跨域进行调整，`baseURL` 将自动加在 `url` 前面//接口跨域
+//axios.defaults.baseURL = '/api'
+//设置超时时间
+axios.defaults.timeout = 8000
+
+//接口错误拦截
+axios.interceptors.response.use(function(response){   //拦截页面返回数据状态码
+  let res = response.data   //返回的数据为response.data，包括status和data
+  if(res.status == 0){      //状态码为0代表登陆成功
+    return res.data
+  }else if(res.status == 10){    //状态码为10代表未登陆，跳转到登陆页面
+      window.location.href = "/#/login"   //跳转到登陆页面
+    }else{
+      alert(res.msg);
+    }
+  });
+ 
+//     return Promise.reject()   //发生错误，promise返回拒绝值
+//   }else{
+//     Message.error(res.msg)
+//     return Promise.reject()   //发生错误，promise返回拒绝值
+//   }
+// },(error)=>{    //拦截http请求状态码（请求发送失败）
+//   let res = error.response
+//   Message.error(res.data.message)
+//   return Promise.reject()   //发生错误，promise返回拒绝值
+// })
+
+Vue.use(VueAxios,axios)
 Vue.config.productionTip = false
 
 new Vue({
