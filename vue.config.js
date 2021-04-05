04550ae5086d725a5f6472c93c4a31d50abc0f6e
sysbian <liushuaidejia@qq.com>
2021-04-05 11:51:26 +0800

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     devServer: {
-        host: '0.0.0.0',
-        // port: 8080,
+        host: '127.0.0.0',
+        port: 8080,
         /*代理*/
         proxy: {
             '/api': {
