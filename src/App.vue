429d51abf0cc28269643fd759ff0fd47f37f0f52
sysbian <liushuaidejia@qq.com>
2021-04-05 19:58:29 +0800

    stroage-json

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,18 @@
 
 <script>
 //import NavHome from './pages/home.vue'
-
+import storage from './storage/index'
 export default {
   name: 'app',
   // components: {
   //   NavHome
   // }
+  mounted(){
+   // storage.setItem('a',1)
+   //storage.setItem('user',{a:1})
+   //storage.setItem('abc',{a:1},'user')
+   storage.clear('a')
+  }
 }
 </script>
 
